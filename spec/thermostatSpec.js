f8ee2c7cdd099ea2bacb92ce218d66eefe3685c4
fwill22 <79596926+fwill22@users.noreply.github.com>
2021-05-07 11:03:10 +0100

    Instantiated thermostat class and set default temp to 20 - first test passes

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,1 +1,17 @@
 'use strict';
+
+describe('Thermostat', function() {
+
+  let thermostat;
+
+  beforeEach(function() {
+    thermostat = new Thermostat;
+  });
+
+  describe('Constructor', function() {
+    it('starts with a default temperature of 20', function() {
+      expect(thermostat.temperature).toEqual(20)
+    });
+  });
+ 
+});
